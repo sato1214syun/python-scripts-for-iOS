1bf9f6a8712080ab24df22bb3e35ea799efb2cd9
sato1214syun <sato1214syun@gmail.com>
2020-12-07 01:07:25 +0900

    temp

--- dl-FRIENDS-subtitle/main.py
+++ dl-FRIENDS-subtitle/main.py
@@ -2,26 +2,14 @@ import os
 import pickle
 import re
 
-# import requests as rq
-# from bs4 import BeautifulSoup as bs
-from requests.models import Response
-from requests_html import HTMLSession
+import requests as rq
+from bs4 import BeautifulSoup as bs
 
 base = os.path.dirname(os.path.abspath(__file__))
 url = r"https://sitcom-friends-eng.seesaa.net/article/388471106.html"
 rel_res_pickle_path = r"./res.pickle"
 res_pickle_path = os.path.normpath(os.path.join(base, rel_res_pickle_path))
 
-session: HTMLSession = HTMLSession()
-res: Response = session.get(url)
-res.html.render()
-res.html.encoding("Shift-JIS")
-
-
-list = res.html.find("a")
-pass
-
-"""
 base = os.path.dirname(os.path.abspath(__file__))
 url = r"https://sitcom-friends-eng.seesaa.net/article/388471106.html"
 
@@ -32,11 +20,30 @@ if os.path.exists(res_pickle_path):
     with open(res_pickle_path, "rb") as f:
         res = pickle.load(f)
 else:
-    res = rq.get(url)
-    with open(res_pickle_path, "wb") as f:
-        pickle.dump(res, f)
+    headers_dict = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+    }
+    res = rq.get(url, headers=headers_dict)
+    print(res.status_code)
+    if res.status_code == 200:
+        with open(res_pickle_path, "wb") as f:
+            pickle.dump(res, f)
 
-soup = bs(res.content, "lxml")
-list = soup.find_all("a", text=re.compile(".*"))
+
+soup: bs = bs(res.content, "lxml")
+title = soup.title.text
+regex_pattern = r".+フレンズ(\d)-(\d+)[あ-ん]+(\d+).+"
+match_group_list = re.match(regex_pattern, title)
+season = match_group_list.group(1)
+chapter = match_group_list.group(2)
+part = match_group_list.group(3)
+print("season: ", match_group_list.group(1))
+print("chapter: ", match_group_list.group(2))
+print("part: ", match_group_list.group(3))
+list = soup.find_all(
+    "a",
+    text=re.compile(
+        ".+フレンズ{}-{}[あ-ん]+{}.+".format(str(season), str(chapter), str(int(part) + 1))
+    ),
+)
 print(list)
-"""
