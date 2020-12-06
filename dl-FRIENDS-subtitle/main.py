f30eceabcc76980773d76b465e1f311fc65a380e
sato1214syun <sato1214syun@gmail.com>
2020-12-07 01:45:39 +0900

    temp

--- dl-FRIENDS-subtitle/main.py
+++ dl-FRIENDS-subtitle/main.py
@@ -6,25 +6,21 @@ import requests as rq
 from bs4 import BeautifulSoup as bs
 
 base = os.path.dirname(os.path.abspath(__file__))
-url = r"https://sitcom-friends-eng.seesaa.net/article/388471106.html"
+fist_part_url = r"https://sitcom-friends-eng.seesaa.net/article/388471106.html"
 rel_res_pickle_path = r"./res.pickle"
 res_pickle_path = os.path.normpath(os.path.join(base, rel_res_pickle_path))
 
-base = os.path.dirname(os.path.abspath(__file__))
-url = r"https://sitcom-friends-eng.seesaa.net/article/388471106.html"
-
-
-rel_res_pickle_path = r"./res.pickle"
-res_pickle_path = os.path.normpath(os.path.join(base, rel_res_pickle_path))
+user_agent = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/78.0.3904.97 Safari/537.36"
+)
+headers_dict = {"User-Agent": user_agent}
 if os.path.exists(res_pickle_path):
     with open(res_pickle_path, "rb") as f:
         res = pickle.load(f)
 else:
-    headers_dict = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
-    }
-    res = rq.get(url, headers=headers_dict)
-    print(res.status_code)
+    res = rq.get(fist_part_url, headers=headers_dict)
     if res.status_code == 200:
         with open(res_pickle_path, "wb") as f:
             pickle.dump(res, f)
@@ -32,18 +28,30 @@ else:
 
 soup: bs = bs(res.content, "lxml")
 title = soup.title.text
-regex_pattern = r".+フレンズ(\d)-(\d+)[あ-ん]+(\d+).+"
+regex_pattern = r".+フレンズ(\d)-(\d+)[あ-ん]+(\d+).*"
 match_group_list = re.match(regex_pattern, title)
 season = match_group_list.group(1)
-chapter = match_group_list.group(2)
+episode = match_group_list.group(2)
 part = match_group_list.group(3)
 print("season: ", match_group_list.group(1))
-print("chapter: ", match_group_list.group(2))
+print("episode: ", match_group_list.group(2))
 print("part: ", match_group_list.group(3))
-list = soup.find_all(
-    "a",
-    text=re.compile(
-        ".+フレンズ{}-{}[あ-ん]+{}.+".format(str(season), str(chapter), str(int(part) + 1))
-    ),
-)
-print(list)
+
+part_link_list = [fist_part_url]
+while True:
+    part = str(int(part) + 1)
+    print("part:", part)
+    next_part_info = soup.find(
+        "a", text=re.compile(".+フレンズ{}-{}[あ-ん]+{}.*".format(season, episode, part))
+    )
+    if next_part_info:
+        # time.sleep(2)
+        next_part_url = next_part_info.attrs["href"]
+        part_link_list.append(next_part_url)
+        res = rq.get(next_part_url, headers=headers_dict)
+        print(res.status_code)
+        soup: bs = bs(res.content, "lxml")
+    else:
+        break
+
+print(part_link_list)
