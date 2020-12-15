a74e429850b7594b361930e0813c28d43e25ee31
佐藤隼介 <sato1214syun@gmail.com>
2020-12-15 14:07:46 +0900

    Run http-server in background

--- dl-FRIENDS-subtitle/RunHttpServer.py
+++ dl-FRIENDS-subtitle/RunHttpServer.py
@@ -2,11 +2,16 @@ import http.server
 import socketserver
 import os
 
+import background as bg
+
 PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 os.chdir(os.path.join(os.path.dirname(__file__), "templates"))
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+
+with bg.BackgroundTask() as b:
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+    
